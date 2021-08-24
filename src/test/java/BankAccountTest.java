b26587385ebcbea8465f5eca2f35200bea2b455b
Michael Groves <michaeliangroves@gmail.com>
2021-08-24 17:41:43 +0100

    Proj setup

---
+++ src/test/java/BankAccountTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BankAccountTest {
+    @Test
+    void testAccountHoldsABalance() {
+        BankAccount subject = new BankAccount();
+        // String result = subject.generate(1);
+        // assertEquals("1", result);
+    }
+
+}
