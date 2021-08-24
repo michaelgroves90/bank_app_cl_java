8554977c0efc936a71123c400b1091d545093ddb
Michael Groves <michaeliangroves@gmail.com>
2021-08-24 17:53:06 +0100

    Build: Test written and build passed for balance attr

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,16 @@
+public class BankAccount {
+
+    public Integer balance;
+
+    public BankAccount() {
+        balance = 0;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+}
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -5,9 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BankAccountTest {
     @Test
     void testAccountHoldsABalance() {
-        BankAccount subject = new BankAccount();
-        // String result = subject.generate(1);
-        // assertEquals("1", result);
+        BankAccount account = new BankAccount();
+        Integer balance = account.getBalance();
+        assertEquals(0, balance);
     }
 
 }
